3bb01701018704620da7070a0087f61ef3fe4415
⚡ Davi <davisantosdias@protonmail.com>
2024-08-12 11:34:14 -0300

    init project

---
+++ Src/Controller/uLogin.Controller.pas
@@ -0,0 +1,47 @@
+﻿unit uLogin.Controller;
+
+interface
+
+uses
+  SysUtils, UnitSection.Controller;
+type
+TLoginController = class
+  private
+  FEmail: string;
+  FSenha: string;
+  public
+  constructor Create(const Email, Senha: string);
+  function usuarioValido(out mensagemErro: string): Boolean;
+  property Email: string read FEmail write FEmail;
+  property Senha: string read FSenha write FSenha;
+end;
+implementation
+
+{ TLoginController }
+
+constructor TLoginController.Create(const Email, Senha: string);
+begin
+  FEmail := email;
+  FSenha := Senha;
+end;
+
+function TLoginController.usuarioValido(out mensagemErro: string): Boolean;
+begin
+
+  if (FEmail = '')  Or (FSenha = '') then
+  begin
+    mensagemErro := 'Os campos (Email e Senha) não pode ser vazio, por favor preencha.';
+    Result := false;
+    Exit
+  end;
+
+  Result := (FEmail = 'admin') and (FSenha = 'admin');
+
+  if not Result then
+      mensagemErro := 'Usuario invalido, revise suas credenciais e tente novamente'
+  else
+      mensagemErro := '';
+
+end;
+
+end.
---
+++ Src/View/UnitProdutos.dfm
@@ -0,0 +1,15 @@
+object frmProdutos: TfrmProdutos
+  Left = 0
+  Top = 0
+  Caption = 'ITS Vendas | Produtos'
+  ClientHeight = 734
+  ClientWidth = 1062
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  OnResize = FormResize
+  TextHeight = 15
+end
